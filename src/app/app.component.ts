2f340cfd89d0b767cf5c403cb23661d9cbfecaa4
Lauro Otavio Urbano <lauro@outlook.com.br>
2023-08-15 23:47:48 -0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'funcionarios_frontend';
+  title = 'Front-end da Aplicação ';
 }
