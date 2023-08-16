fc3288ff512e50a93e8abc7b7bd594ff81012277
Lauro Otavio Urbano <lauro@outlook.com.br>
2023-08-15 23:58:21 -0300

    adicionando a rota padrão

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '',  redirectTo: '/funcionarios', pathMatch: 'full' },
   { path: 'funcionarios', loadChildren: () => import('./funcionarios/funcionarios.module').then(m => m.FuncionariosModule) },
   { path: 'departamentos', loadChildren: () => import('./departamentos/departamentos.module').then(m => m.DepartamentosModule) }
 ];
